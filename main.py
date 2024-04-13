68744172562fd8005fb7c0da705f4339a488238c
Patel Meet <mptl2607@gmail.com>
2024-04-13 10:31:03 -0400

    i add the comments and add all details  in track list.

--- main.py
+++ main.py
@@ -2,33 +2,45 @@ import json
 from datetime import timedelta
 import pandas as pd
 
+# read file from Songs.json as file and store data in spotify_json
 with open("Songs.json", 'r') as file:
     spotify_json = json.load(file)
 
-# duration_time = spotify_json['tracks'][0]['duration_ms'] #280000
-
-# total_sec = duration_time / 1000
-# min = int(total_sec / 60)
-# sec = int(total_sec % 60)
-# print(f"The duration time is {min} minute and {sec} second")
-
-def duration(ms):
+# create function for convert duration time from millisecond to minutes
+def duration_min(ms):
     t  = timedelta(milliseconds= ms)
     min = int(t.seconds / 60)
-    sec = int(t.seconds % 60)
-    return f"{min}:{sec:02d}"
-# print(duration(900000))
+    return min
 
+# create function for convert duration time from millisecond to second
+def duration_sec(ms):
+    t  = timedelta(milliseconds= ms)
+    sec = int(t.seconds % 60)
+    return sec
+
+#create empty list to store data
 track_list = []
 
+#run for loop for read data from spotify_json['tracks'] and append in track_list = [] list
 for i in spotify_json['tracks']:
     track = {
-        'Playlist name' : spotify_json['playlist_name'],
-        'Track name': i['track_name'],
-        'Duration(min:sec)': duration(i['duration_ms'])
+        'Playlist Name' : spotify_json['playlist_name'],
+        'Description' :spotify_json['description'],
+        'User Name':spotify_json['creator']['username'] ,
+        "Email":spotify_json['creator']['email'],
+        'Track Name': i['track_name'],
+        "Artist": i['artist'],
+        "Album": i['album'],
+        "Album Name": i['album']['name'],
+        "Release Date": i['album']['release_date'],
+        'Duration(min)': duration_min(i['duration_ms']),
+        'Duration(sec)': duration_sec(i['duration_ms']),
+        "Popularity": i['popularity'],
+        "Genres": i['genres'],
+        "Explicit Content": i['explicit_content']
     }
     track_list.append(track)
-df = pd.DataFrame(track_list)
-df.to_csv('songs.csv', index = False)
+
+
 
 
