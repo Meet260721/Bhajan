9145385c7d612ccb444329283bb05951813f6d38
Patel Meet <mptl2607@gmail.com>
2024-04-24 18:23:31 -0400

    We add more data in json file. then create for loop for playlist. removed 2 column in track table and create one new column, and print value for duration time. create one new table for relationship between track and genres.

--- main.py
+++ main.py
@@ -18,74 +18,96 @@ connection = psycopg2.connect(
 )
 curr = connection.cursor()
 # create function for convert duration time from millisecond to minutes
-def duration_min(ms):
+def duration_time(ms):
     t = timedelta(milliseconds= ms)
     min = int(t.seconds / 60)
-    return min
+    sec = int(t.seconds % 60)
+    return f"{min}.{sec:02d}"
+
 
 # create function for convert duration time from millisecond to second
-def duration_sec(ms):
-    t  = timedelta(milliseconds= ms)
-    sec = int(t.seconds % 60)
-    return sec
+# def duration_sec(ms):
+#     t  = timedelta(milliseconds= ms)
+#     sec = int(t.seconds % 60)
+#     return sec
 
-#Insert values from the  json data to  playlist table  in database
-curr.execute(sql.SQL("""
-    INSERT INTO playlist (playlist_name, description, creator_username, creator_email)
-    VALUES (%s, %s, %s, %s)
-    ON CONFLICT (playlist_name, creator_username) DO NOTHING
-    """),
-    (
-        spotify_json['playlist_name'],
-        spotify_json['description'],
-        spotify_json['creator']['username'],
-        spotify_json['creator']['email']
-    ))
-
-#Insert values from the  json data to  track table  in database
-for track in spotify_json['tracks']:
-    curr.execute(
-        sql.SQL("SELECT playlist_id FROM playlist WHERE playlist_name = %s AND creator_username = %s"),
-        (spotify_json['playlist_name'], spotify_json['creator']['username'])
-    )
-    result = curr.fetchone()
-
-    playlist_id = result[0] if result else None
-
-    if playlist_id is not None:
-        curr.execute(sql.SQL("""
-            INSERT INTO track (track_name,playlist_id, artist, album_name, release_date, duration_min, popularity, explicit_content, duration_sec)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s,%s)
-            ON CONFLICT (track_name, artist) DO NOTHING
-            """),
-            (
-                track['track_name'],
-                playlist_id,
-                track['artist'],
-                track['album']['name'],
-                track['album']['release_date'],
-                duration_min(track['duration_ms']),
-                track['popularity'],
-                # track['genres'],
-                track['explicit_content'],
-                duration_sec(track['duration_ms'])
-            ))
-
-# Extract genre names from the JSON data
 genre_names = []
-for track in spotify_json['tracks']:
-    genre_names.extend(track['genres'])
-
-# Remove duplicate genre
-genre_names = list(set(genre_names))
-
-# Insert genre names into the genre table
-for genre_name in genre_names:
+#Insert values from the  json data to  playlist table  in database
+for playlist_data in spotify_json:
     curr.execute(sql.SQL("""
-        INSERT INTO genre (genre_name)
-        VALUES (%s)
-        ON CONFLICT (genre_name) DO NOTHING
-         """), (genre_name,))
+        INSERT INTO playlist (playlist_name, description, creator_username, creator_email)
+        VALUES (%s, %s, %s, %s)
+        ON CONFLICT (playlist_name, creator_username) DO NOTHING
+        """),
+        (
+            playlist_data['playlist_name'],
+            playlist_data['description'],
+            playlist_data['creator']['username'],
+            playlist_data['creator']['email']
+        ))
+
+    #Insert values from the  json data to  track table  in database
+    for track in playlist_data['tracks']:
+        genres_str = ','.join(track['genres'])
+        curr.execute(
+            sql.SQL("SELECT playlist_id FROM playlist WHERE playlist_name = %s AND creator_username = %s"),
+            (playlist_data['playlist_name'], playlist_data['creator']['username'])
+        )
+        result = curr.fetchone()
+
+        playlist_id = result[0] if result else None
+
+        if playlist_id is not None:
+            curr.execute(sql.SQL("""
+                INSERT INTO track (track_name,playlist_id, artist, album_name, release_date, duration_time, popularity, genres, explicit_content)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT (track_name, artist) DO NOTHING
+                
+                """),
+                (
+                    track['track_name'],
+                    playlist_id,
+                    track['artist'],
+                    track['album']['name'],
+                    track['album']['release_date'],
+                    duration_time(track['duration_ms']),
+                    track['popularity'],
+                    genres_str,
+                    track['explicit_content']
+                ))
+            curr.execute(sql.SQL("""
+                        SELECT track_id FROM track 
+                        WHERE track_name = %s AND artist = %s
+                    """), (
+                track['track_name'],
+                track['artist']
+            ))
+            track_id = curr.fetchone()[0]
+
+    # Extract genre names from the JSON data
+
+    for track in playlist_data['tracks']:
+        genre_names.extend(track['genres'])
+
+    # Remove duplicate genre
+    genre_names = list(set(genre_names))
+
+    # Insert genre names into the genre table
+    for genre_name in genre_names:
+        curr.execute(sql.SQL("""
+            INSERT INTO genre (genre_name)
+            VALUES (%s)
+            ON CONFLICT (genre_name) DO NOTHING
+             """), (genre_name,))
+
+        curr.execute(sql.SQL("""
+                        INSERT INTO track_genres (track_id, genre_id)
+                        SELECT %s, genre_id FROM genre
+                        WHERE genre_name = %s
+                    """), (
+            track_id,
+            genre_name
+        ))
 
 
 connection.commit()
@@ -93,4 +115,3 @@ connection.commit()
 # Close the cursor and connection
 curr.close()
 connection.close()
-
