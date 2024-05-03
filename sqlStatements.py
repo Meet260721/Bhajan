860a64a2cf01b4927a70333c583fc4b2774bd7c2
Patel Meet <mptl2607@gmail.com>
2024-05-03 12:40:08 -0400

    we wrote unit test cases in unit.py file for the methods in validation.py.

--- sqlStatements.py
+++ sqlStatements.py
@@ -47,55 +47,55 @@ track_genres_create_table_query = """
                                 CONSTRAINT track_genres_unique_constraint UNIQUE (track_id, genre_id))
 """
 
-#Query for insert value in playlist table
-playlist_insert_query ="""
+# Query for insert value in playlist table
+playlist_insert_query = """
                         INSERT INTO playlist (playlist_name, description, creator_username, creator_email)
                         VALUES (%s, %s, %s, %s)
                         ON CONFLICT (playlist_name, creator_username) DO NOTHING
                      """
 
-#Query for retrieve playlist_id
-playlist_id_retrieve_query ="""
+# Query for retrieve playlist_id
+playlist_id_retrieve_query = """
                                 SELECT playlist_id FROM playlist 
                                 WHERE playlist_name = %s AND creator_username = %s
                             """
 
-#Query for insert value in album table
+# Query for insert value in album table
 album_insert_query = """
                         INSERT INTO album (name, release_date)
                         VALUES (%s, %s)
                         ON CONFLICT DO NOTHING
                     """
 
-#Query for retrieve album_id
+# Query for retrieve album_id
 
 album_id_retrieve_query = """
                            SELECT album_id FROM album
                            WHERE name = %s AND release_date = %s
                            """
 
-#Query for insert value in track table
+# Query for insert value in track table
 track_insert_query = """
                         INSERT INTO tracks (track_name,playlist_id, artist, album_name, release_date, duration_time, popularity, genres, explicit_content, album_id)
                         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                         ON CONFLICT (track_name, artist) DO NOTHING
                         """
 
-#Query for retrieve track_id
+# Query for retrieve track_id
 
 track_id_retrieve_qurey = """
                             SELECT track_id FROM tracks
                             WHERE track_name = %s AND artist = %s
                             """
 
-#Query for insert value in genres table
+# Query for insert value in genres table
 genres_insert_query = """
                         INSERT INTO genres (genre_name)
                         VALUES (%s)
                         ON CONFLICT (genre_name) DO NOTHING
                      """
 
-#Query for retrieve genres_id
+# Query for retrieve genres_id
 
 track_genres_insert_query = """
                             INSERT INTO track_genres (track_id, genre_id)
